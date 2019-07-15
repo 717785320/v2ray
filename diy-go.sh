29c7b80509d33576c9f4f1d239a154d1cb58a9b7
717785320 <40885345+717785320@users.noreply.github.com>
2019-07-15 10:23:08 +0800

    Update diy-go.sh

--- diy-go.sh
+++ diy-go.sh
@@ -126,7 +126,7 @@ downloadV2Ray(){
     if [[ "${DIST_SRC}" == "jsdelivr" ]]; then
         DOWNLOAD_LINK="https://cdn.jsdelivr.net/gh/v2ray/dist/v2ray-linux-${VDIS}.zip"
     else
-        DOWNLOAD_LINK="https://github.com/717785320/v2ray/releases/download/${NEW_VER}/v2ray-linux-${VDIS}.zip"
+        DOWNLOAD_LINK="https://github.com/717785320/v2ray/releases/download/v4.20.0/v2ray-linux-${VDIS}.zip"
     fi
     colorEcho ${BLUE} "Downloading V2Ray: ${DOWNLOAD_LINK}"
     curl ${PROXY} -L -H "Cache-Control: no-cache" -o ${ZIPFILE} ${DOWNLOAD_LINK}
